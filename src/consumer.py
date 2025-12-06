80f53b2169e715aae6b236eb672db7f2d8191090
s1ush <yashbholeg@gmail.com>
2025-12-06 16:11:38 -0500

    added consumer and data_item as model for producer and consumer

---
+++ src/consumer.py
@@ -0,0 +1,25 @@
+import threading
+import time
+import logging
+from .blocking_queue import BlockingQueue
+
+logger = logging.getLogger(__name__)
+
+class Consumer(threading.Thread):
+    def __init__(self, queue: BlockingQueue, processing_time: float = 0.2):
+        super().__init__()
+        self.queue = queue
+        self.processing_time = processing_time
+
+    def run(self):
+        logger.info("Consumer started.")
+        while True:
+            item = self.queue.take()
+
+            if item.is_poison_pill():
+                logger.info("Poison Pill received. Shutting down Consumer.")
+                break
+
+            logger.info(f"Consuming: {item.payload}")
+            # Simulate processing time
+            time.sleep(self.processing_time)
